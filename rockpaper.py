2a3e411c620386d63b238bbf1ff17ad8dac80972
oxoop <geonnievillegsa@gmail.com>
2026-05-09 10:12:33 -0400

    changed some ui stuff

--- rockpaper.py
+++ rockpaper.py
@@ -1,6 +1,8 @@
 import random
-
-
+from rich.console import Console
+from rich.text import Text
+from rich import print 
+console = Console()
 class rps:
     def __init__(self, debug: bool) -> None:
         self.choices = ["r", "p", "s"]
@@ -18,25 +20,26 @@ class rps:
             print(f"The output is: {self.ai} The type: {type(self.ai)}" )
 
     def GetUserInput(self):
-        playerop = input("""
--------
-|R,P,S|
-|-> """)
+        playerop = console.input("""
+[bold red]-------
+[bold red]|R,P,S|
+[bold blue]|[bold deep_pink]-> """)
+        print("[bold blue]|-----|")
         self.playerop = playerop.lower()
         if self.debug:
             print(f"The player Choose: {self.playerop}, The Type is: {type(self.playerop)}")
     def CheckForRps(self):
 
         if self.ai == self.playerop:
-            print("You tied")
+            print("[bold white]|You tied")
         elif self.winrule[self.playerop] == self.ai:
-            print("You win!")
+            print("[bold blue]|[italic green]You win!")
         else:
-            print("|You lose")
-        print(f"|They choose {self.ai}")
-        print("\n  's = scissors, p = paper, r = rock'")
+            print("[bold blue]|[italic red]You lose")
+        print(f"[bold blue]|[bold yellow]They choose {self.ai}")
+        print("\n  [italic]'s = scissors, p = paper, r = rock'")
+        
 game = rps(False)
 game.getrandom([1,1,1])
 game.GetUserInput()
 game.CheckForRps()
-
